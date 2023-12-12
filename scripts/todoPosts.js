8151ffe5d52ddde2715c0cb6c0b8c3d83e45298b
&blueblood <khashchuk.denis@gmail.com>
2023-12-12 21:12:50 +0300

    feat: localstorage add

---
+++ scripts/todoPosts.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const themeForm = document.getElementById("postsTodo");
+    const bookListContainer = document.getElementById("themesContainer");
+
+    function addThemeToContainer(theme, explanation) {
+        const themeEntry = document.createElement("div");
+        themeEntry.classList.add("theme-entry");
+        themeEntry.innerHTML = `<strong>${theme}</strong> - ${explanation} <button class="delete-button">Удалить</button>`;
+        bookListContainer.appendChild(themeEntry);
+
+        const deleteButton = themeEntry.querySelector(".delete-button");
+        deleteButton.addEventListener("click", function() {
+            bookListContainer.removeChild(themeEntry);
+            updateLocalStorage();
+        });
+    }
+
+    function updateLocalStorage() {
+        const bookEntries = Array.from(bookListContainer.querySelectorAll(".book-entry"));
+        const booksData = bookEntries.map(bookEntry => {
+            const title = bookEntry.querySelector("strong").textContent;
+            const author = bookEntry.textContent.split(" - ")[1];
+            return { title, author };
+        });
+        localStorage.setItem("themes", JSON.stringify(booksData));
+    }
+
+    themeForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const themeGet = document.getElementById("theme").value;
+        const explanationGet = document.getElementById("explanations").value;
+
+        addThemeToContainer(themeGet, explanationGet);
+
+        const themeData = {
+            title: themeGet,
+            author: explanationGet
+        };
+        let themes = localStorage.getItem("themes") ? JSON.parse(localStorage.getItem("themes")) : [];
+        themes.push(themeData);
+        localStorage.setItem("themes", JSON.stringify(themes));
+
+        themeForm.reset();
+    });
+
+    let savedThemes = localStorage.getItem("themes");
+    if (savedThemes) {
+        savedThemes = JSON.parse(savedThemes);
+        savedThemes.forEach(book => {
+            addThemeToContainer(book.title, book.author);
+        });
+    }
+});
